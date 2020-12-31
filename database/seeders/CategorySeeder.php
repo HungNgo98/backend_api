c0b4656a2c9db4ec193b224f27be7a87b8a0495a
sonth <truongson280797@gmail.com>
2020-12-31 17:37:33 +0700

    son

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Factory::create('id');
+        for ($i=0; $i < 10; $i++) {
+            $data = [
+                'name'     => $faker->name,
+                'description'     => $faker->description,
+                'created_at'=> $faker->dateTimeBetween('-2 month', '-1 days')->format('Y-m-d H:i:s'),
+                'update_at'=> $faker->dateTimeBetween('-2 month', '-1 days')->format('Y-m-d H:i:s')
+            ];
+            $this->db->table('categories')->insert($data);
+        }
+    }
+}
