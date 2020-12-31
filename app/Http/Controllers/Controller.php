049b956a37452f3828eadcd40a11d96d456ac6ed
hoangvd <vudinhhoang358@gmailo.com>
2020-12-31 11:15:16 +0700

    update controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,7 +11,7 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
     //ábdhasbfhashb
-    //kasdjasbfbaj
+    //kasdjasbfbajghfgbcdbgdgftbtfbfbfbfbvf
     //kasfhjhasfj
     //áhdahsba
 }
