cdc245ab4ec09a8a48d9aee0306f93c7864d3188
sonth <truongson280797@gmail.com>
2021-01-04 17:02:51 +0700

    son

---
+++ app/Http/Requests/CategoryRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Response;
+
+class CategoryRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+    public function rules()
+    {
+        return [
+            'name'=>'required',
+            'description'=>'required',
+        ];
+    }
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            response()->json($validator->errors()->toArray(), Response::HTTP_BAD_REQUEST)
+        );
+    }
+
+}
